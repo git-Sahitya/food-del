0626ad09aa50fa7b96da54b1600fa6db64b96e0c
git-Sahitya <sahitya5289@gmail.com>
2025-04-29 10:24:57 +0530

    setup the backend for this food del and install some packages

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+
+
+//app config 
+ const app = express()
+ const port = 4000
+
+ //middleware
+ app.use(express.json())
+ app.use(express.urlencoded({extended:true}))
+ app.use(cors())   
+
+app.get('/',(req,res)=>{
+   res.send('Api working')
+})
+
+ app.listen(port , ()=>{
+    console.log(`Server is running on port ${port}`);
+    
+ })
