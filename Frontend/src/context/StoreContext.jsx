4ed060dc323f376fe4dba688028cdfeec8f5382e
git-Sahitya <sahitya5289@gmail.com>
2025-05-01 10:19:11 +0530

    Add axios for API calls and implement user authentication in LoginPopup; update FoodItem and Cart components to use dynamic image URLs; enhance Navbar with profile dropdown and logout functionality; refactor StoreContext for food list fetching.

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+import axios from 'axios'
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000"
+  const [token , setToken] = useState("")
+  const [food_list, setFoodList] = useState([])
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -29,6 +32,22 @@ const StoreContextProvider = (props) => {
     }
     return totalAmount
   }
+  const fetchFoodList = async ()=>{
+  const response = await axios.get(`${url}/api/food/list`)
+  console.log(response);
+  setFoodList(response.data.data)
+  }
+
+  useEffect(()=>{
+    async function loadData() {
+      await fetchFoodList()
+    }
+  if (localStorage.getItem("token")) {
+    setToken(localStorage.getItem("token"))
+  }
+  loadData()
+ 
+  },[])
   
 
   const contextValue = {
@@ -37,7 +56,10 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
   };
 
   return (
