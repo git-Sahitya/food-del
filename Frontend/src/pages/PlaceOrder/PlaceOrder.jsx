1a495626307577558d0da93aeeb5a05e6b62dc3e
git-Sahitya <sahitya5289@gmail.com>
2025-05-03 09:20:03 +0530

    here frontend is completed!!

--- Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ Frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
-
+import {useNavigate} from "react-router-dom"
 const PlaceOrder = () => {
   
+  const navigate = useNavigate()
   const { getTotalCartAmount, token, food_list, cartItems, url } =
     useContext(StoreContext);
   const [data, setData] = useState({
@@ -51,6 +52,14 @@ const PlaceOrder = () => {
       
     }
   };
+
+  useEffect(()=>{
+if (!token) {
+  navigate('/cart')
+}else if(getTotalCartAmount()=== 0){
+  navigate('/cart')
+}
+  },[token])
 
   return (
     <form onSubmit={placeOrder} className="place-order">
